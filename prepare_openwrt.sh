1167ae135dad2b3a8a0124ce1314d6ee4f1ce994
mytechpg <mytechp@gmail.com>
2016-01-02 22:14:22 +0100

    added patches and script to prepare the environment

---
+++ prepare_openwrt.sh
@@ -0,0 +1,27 @@
+!#/bin/sh
+
+echo "Downloading OpenWRT ..."
+
+git clone git://git.openwrt.org/15.05/openwrt.git
+
+echo "OpenWRT downloaded"
+
+echo "Download MPTCP kernel patch ..."
+git clone -b linux_3.18.20-mptcp_0.90 https://github.com/mytechpg/mptcp-kernel-patch.git
+
+echo "Patch downloaded"
+
+cd openwrt
+
+echo "Preparing packages ..."
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+echo "Packages ready"
+
+echo "Applyng patches"
+cp -R ../package .
+cp ../mptcp-kernel-patch/mptcp_linux_kernel.patch target/linux/generic/patches-3.18/622-mptcp_linux_kernel.patch
+
+echo "System ready!"
+
